db0e3b366dfbc4a0a181e0b66890a2f7cce0883b
gmae199boy <kim88594544@gmail.com>
2020-06-19 16:52:08 +0900

    commit

---
+++ network/cc_tea.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#instruction=$1
+#version=$2
+
+set -ev
+
+#chaincode install
+docker exec cli peer chaincode install -n donation -p /home/bstudent/dev/donation/contract/donation/donation
+#chaincode instatiate
+docker exec cli peer chaincode instantiate -n donation -C mychannel -c '{"Args":[]}' -P 'OR ("Org1MSP.member", "Org2MSP.member","Org3MSP.member")'
+sleep 5
+#chaincode invoke user1
+docker exec cli peer chaincode invoke -n donation -C mychannel -c '{"Args":["addPH","a","kim","19910503","seoul","01022222222","1","1212","asfas"]}'
+sleep 5
+#chaincode query user1
+#docker exec cli peer chaincode query -n donation -C mychannel -c '{"Args":["readRating","user1"]}'
+
+#chaincode invoke add rating
+#docker exec cli peer chaincode invoke -n donation -C mychannel -c '{"Args":["addRating","user1","p1","5.0"]}'
+#sleep 5
+
+echo '-------------------------------------END-------------------------------------'
